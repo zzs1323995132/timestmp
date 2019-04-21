ff7b1b17f59a32be309477a001174ee588b795b6
zzs <1323995132@qq.com>
2019-04-21 21:00:25 +0800

    zzs

--- timestamp/app/src/main/java/com/example/zzs/timestamp/MainActivity.java
+++ timestamp/app/src/main/java/com/example/zzs/timestamp/MainActivity.java
@@ -17,7 +17,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Created by zhouzhangshan on 2016/10/1.
+ * Created by zhouzhan电话号码gshan on 2016/10/1.
  */
 public class MainActivity extends AppCompatActivity {
     private TextView text3;
